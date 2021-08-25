a297a1fffb10f13b2ac3a1cddc248b53244e982b
ivannaht <ivhontaryk@gmail.com>
2021-08-25 22:28:49 +0300

    "Added list of tasks"

--- index.rb
+++ index.rb
@@ -5,3 +5,40 @@ puts('one') # A string literal
 puts("two") # Another string literal
 puts(/three/) # A regular expression literal
 
+=begin
+Install Ruby if not installed.
+Create a new Ruby file with a .rb extension. Example: index.rb
+Call the created file in the console using ruby
+Open the created file with any favorite editor
+Write a few lines and comment on them
+Add to the output file of the basic 'edge' literals (Fixnum, Float, String)
+Declare multiple variables with readable names and display these variables (can also be Fixnum, Float, String)
+Try to declare variables with invalid names, run the file (item 5), comment out the lines that cause exceptions (errors)
+Declare the expression of type total = a + b with the transfer of the second operand to a new line, and make sure that Ruby continues to build the expression if at the end of the line finds any operator
+Declare an expression of type total = a + b with the second operand with the operator on a new line, and make sure that Ruby ignores the next line if it sees the completed expression
+total = a + b Move + b to a new line using the special operator '\', and make sure that Ruby interprets two expressions in two lines as one expression
+Write an expression in which to divide two integers (for example 5 and 2, or 7 and 3), but such two numbers, the division of which would give the remainder, get the wrong answer
+Rewrite the expression with division so that the result was correct.
+Try dividing any numeric value by 0 (get error)
+Divide the fractional number by 0
+Write an expression using the "remainder of division" operator
+Declare a variable, and elevate it to a power using the exponentiation operator
+Declare a string literal in single quotes
+Declare a string literal in single quotes using the characters you want to screen
+Declare a string literal with interpolation
+Declare a long string literal by moving it to a new line using the "\" symbol
+Declare a string literal using string modifiers%,% q% Q.
+Interpolate any variable in the string declared with% Q
+Declare two lines and add them
+Try adding an integer value to the string.
+Declare two identical lines and compare them.
+Declare a string and duplicate it n times with the "*" character
+Declare a string and add any text to it using the "<<" operator
+Declare the string 'I love Ruby' using the '[]' method to get the 'R' character and the 'I' character in two ways.
+Declare the string 'Ruby' using the method "[]" to get the value nil
+Declare the string 'Hello Ruby' and using the operator "[]" passing the substring 'Ruby', in two ways
+Declare the line 'Ruby is awesome' and remove 'is' from it. The result should be a string 'Ruby awesome'
+Declare the string 'RUBY' and convert it to 'ruby' using the method. (You need to find the necessary method yourself)
+Declare the line 'Ruby courses', and using the method, print the number of characters in the line.
+Declare the string 'Ruby' using the required method to print the string 'ybuR'
+=end
