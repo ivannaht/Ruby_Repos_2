9a06d96ef1b78dd182583411eb453a75aee9eb92
ivannaht <ivhontaryk@gmail.com>
2021-08-25 21:51:31 +0300

    "Added index.rb"

---
+++ index.rb
@@ -0,0 +1,7 @@
+#Task 6
+puts(1)	# An integer literal
+puts(1.0)	# A floating-point literal
+puts('one') # A string literal
+puts("two") # Another string literal
+puts(/three/) # A regular expression literal
+
